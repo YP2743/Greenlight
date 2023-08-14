0e4e50ee8a9cb0adb5877a9206353aa8951d3ff4
Truong Minh Duc <ypvt123@gmail.com>
2023-08-14 17:24:01 +0700

    database connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
 
@@ -16,11 +19,45 @@ const version = "1.0.0"
 type config struct {
 	port string
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
 	config config
 	logger *log.Logger
+}
+
+// Singleton pattern to make sure that only one connection pool exists.
+type postgres struct {
+	pool *pgxpool.Pool
+}
+
+var (
+	pgInstance *postgres
+	pgOnce     sync.Once
+)
+
+func openDB(cfg config) (*postgres, error) {
+	var err error
+	pgOnce.Do(func() {
+		var db *pgxpool.Pool
+		db, err = pgxpool.New(context.Background(), cfg.db.dsn)
+		if err != nil {
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		// Check connection within the 5-second deadline.
+		err = db.Ping(ctx)
+		if err == nil {
+			pgInstance = &postgres{pool: db}
+		}
+	})
+	return pgInstance, nil
 }
 
 func main() {
@@ -34,9 +71,17 @@ func main() {
 
 	flag.StringVar(&cfg.port, "port", os.Getenv("PORT"), "API server port")
 	flag.StringVar(&cfg.env, "env", os.Getenv("ENVIRONMENT"), "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DB_URL"), "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.pool.Close()
+	logger.Printf("database connection pool established")
 
 	app := &application{
 		config: cfg,
